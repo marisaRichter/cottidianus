b429f2419fc87a97410c214127e9fe46416025f3
Jessica Dagostini <jessicadagostini@gmail.com>
2017-12-03 14:05:36 -0200

    Adição da nav nos forms

--- resources/views/turmas/create.blade.php
+++ resources/views/turmas/create.blade.php
@@ -1,6 +1,52 @@
 @extends('app')
 
 @section('content')
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar bar1"></span>
+            <span class="icon-bar bar2"></span>
+            <span class="icon-bar bar3"></span>
+        </button>
+        <a href ="{{ route('turmas') }}"class="navbar-brand ti-arrow-left"></a>
+        <a class="navbar-brand" href="#">Turmas</a>
+    </div>
+    <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="ti-panel"></i>
+    <p>Stats</p>
+                </a>
+            </li>
+            <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="ti-bell"></i>
+                        <p class="notification">5</p>
+      <p>Notifications</p>
+      <b class="caret"></b>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Notification 1</a></li>
+                    <li><a href="#">Notification 2</a></li>
+                    <li><a href="#">Notification 3</a></li>
+                    <li><a href="#">Notification 4</a></li>
+                    <li><a href="#">Another notification</a></li>
+                  </ul>
+            </li>
+<li>
+                <a href="#">
+    <i class="ti-settings"></i>
+    <p>Settings</p>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+</div>
+</nav>
   <div class="content">
     <div class="container-fluid">
       <div class="card">
@@ -27,7 +73,7 @@
           {!! Form::text('descricao', null, ['class'=>'form-control']) !!}
         </div>
         <div class="form-group">
-          {!! Form::label('alunos', 'Descrição:') !!}
+          {!! Form::label('alunos', 'Estudantes:') !!}
           {!! Form::select('alunos_turmas[]', $alunos, null, ['id'=>'selectize', 'multiple' => true]) !!}
         </div>
           <div class="form-group">
