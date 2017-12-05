8523167b5267f3b59bd2859f4e662f05eb9372a2
Jessica Dagostini <jessicadagostini@gmail.com>
2017-12-05 11:58:57 -0200

    Remoção nav comportamentos

--- resources/views/comportamentos/index.blade.php
+++ resources/views/comportamentos/index.blade.php
@@ -13,38 +13,6 @@
             </button>
             <a class="navbar-brand" href="#">Comportamentos</a>
         </div>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="ti-panel"></i>
-        <p>Stats</p>
-                    </a>
-                </li>
-                <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="ti-bell"></i>
-                            <p class="notification">5</p>
-          <p>Notifications</p>
-          <b class="caret"></b>
-                      </a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#">Notification 1</a></li>
-                        <li><a href="#">Notification 2</a></li>
-                        <li><a href="#">Notification 3</a></li>
-                        <li><a href="#">Notification 4</a></li>
-                        <li><a href="#">Another notification</a></li>
-                      </ul>
-                </li>
-    <li>
-                    <a href="#">
-        <i class="ti-settings"></i>
-        <p>Settings</p>
-                    </a>
-                </li>
-            </ul>
-
-        </div>
     </div>
 </nav>
 
@@ -54,8 +22,13 @@
       <div class="col-md-12">
           <div class="card">
               <div class="header">
-                  <h4 class="title">Listar Comportamentos</h4>
-                  <p class="category">Aqui você encontrará todas os comportamentos cadastrados</p>
+                    <div class="left">
+                        <h4 class="title">Listar Comportamentos</h4>
+                        <p class="category">Aqui você encontrará todas os comportamentos cadastrados</p>
+                    </div>
+                    <div class="right">
+                        <a href="{{ route('comportamentos.create') }}" class="btn btn-fill btn-success">Novo Comportamento</a>
+                    </div>                  
               </div>
               <div class="content table-responsive table-full-width">
                   <table class="table table-striped">
